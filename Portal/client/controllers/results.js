148e3fafa02baf7418644d0745c2afb60498be4c
unknown <ayost@VAT-AYOST.cb.careerbuilder.com>
2015-07-07 16:20:15 -0400

    empty errors for test

--- Portal/client/controllers/results.js
+++ Portal/client/controllers/results.js
@@ -1,10 +1,13 @@
 'use strict';
 
 app.controller('ErrorCtrl', function($http, $scope, toastr){
-  $scope.results=[{User: {Username: "ayost"}, Database:{Name:"Testdb"}, Error:{Title: "Testing Error Page", Details:"This is only a test"}, Retryable: false, Class:"Info"},
+  $scope.results=[
+                  /*
+                  {User: {Username: "ayost"}, Database:{Name:"Testdb"}, Error:{Title: "Testing Error Page", Details:"This is only a test"}, Retryable: false, Class:"Info"},
                   {User: {Username: "ayost"}, Database:{Name:"Testdb"}, Error:{Title: "Testing Error Page", Details:"This is only a test"}, Retryable: false, Class:"Warning"},
                   {User: {Username: "ayost"}, Database:{Name:"Testdb"}, Error:{Title: "Testing Error Page", Details:"This is only a test"}, Retryable: false, Class:"Error"},
                   {User: {Username: "ayost"}, Database:{Name:"Testdb"}, Error:{Title: "Testing Error Page", Details:"This is only a test"}, Retryable: false, Class:"Success"}
+                  */
                   ];
   $scope.$emit('tabChanged', 4);
   $http.get('https://deadbolt.cbsitedb.net/api/errors/').success(function(data){
