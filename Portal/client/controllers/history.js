608aeef8191a621aa76f4a3536466bd2ec405129
unknown <ayost@VAT-AYOST.cb.careerbuilder.com>
2015-07-08 11:14:02 -0400

    better formatted time and date for history

--- Portal/client/controllers/history.js
+++ Portal/client/controllers/history.js
@@ -5,7 +5,19 @@ app.controller('HistoryCtrl', function($http, $scope, $cookies, $cookieStore, to
   $scope.$emit('tabChanged', 0);
   $http.get('https://deadbolt.cbsitedb.net/api/history/7').success(function(data){
     if(data.Success){
-      $scope.history = data.History;
+      data.History.forEach(function(hist, i){
+        var timestring = hist.Time.toString();
+        var parts = timestring.split('t');
+        var dateparts = parts[0].split('-');
+        var timeparts = parts[1].split('.')[0];
+        var date = dateparts[1]+"/"+dateparts[2]+"/"+dateparts[0];
+        var ftime = timeparts+" UTC";
+        $scope.history.push({
+                            Date: date,
+                            Time: ftime,
+                            Activity: hist.Activity
+                            });
+      });
     }
     else{
       console.log(data.Error);
