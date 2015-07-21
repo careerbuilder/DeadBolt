ac0820e8f1837256ede2f4c31388a4a642cceff1
Adam Yost <Adam.Yost@Careerbuilder.com>
2015-07-21 16:44:22 -0400

    Don't Create if it already exists

--- Portal/tools/mssql_tools.js
+++ Portal/tools/mssql_tools.js
@@ -57,7 +57,7 @@ module.exports = {
                 trans.begin(function(err){
                   var request = new mssql.Request(trans);
                   request.input('username', mssql.NVarChar, user.Username);
-                  request.query("IF Exists (SELECT * FROM syslogins WHERE name= ''" + user.Username + "'') \
+                  request.query("IF NOT Exists (SELECT * FROM syslogins WHERE name= ''" + user.Username + "'') \
                   CREATE Login [" + user.Username + "] WITH password=" + user.SQL_Server_Password + " HASHED, CHECK_POLICY=OFF, CHECK_EXPIRATION=OFF \
                   ELSE ALTER LOGIN [" + user.Username + "] WITH PASSWORD=" + user.SQL_Server_Password + " HASHED, CHECK_POLICY=OFF, CHECK_EXPIRATION=OFF", function(err, records){
                     trans.commit(function(err) {
