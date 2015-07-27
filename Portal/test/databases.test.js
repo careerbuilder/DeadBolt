2f6f18fe7a6e0161e127fc1f9ebe75cd13a27ebd
Adam Yost <Adam.Yost@Careerbuilder.com>
2015-07-27 15:30:10 -0400

    100% database code coverage

--- Portal/test/databases.test.js
+++ Portal/test/databases.test.js
@@ -1,4 +1,4 @@
-describe.only('databases', function(){
+describe('databases', function(){
   var express = require('express');
   var assert = require('assert');
   var rewire = require('rewire');
@@ -9,70 +9,96 @@ describe.only('databases', function(){
   var databases = rewire('../routes/databases.js');
 
   var mock_db = {
-      query: function(){
-        var sql_args = [];
-        var args = [];
-        for(var i=0; i<arguments.length; i++){
-          args.push(arguments[i]);
-        }
-        var callback = args[args.length-1]; //last arg is callback
-        if(args.length > 2){
-          sql_args = args[1];
-        }
-        if(args[0].toUpperCase().search('SELECT ID,') >-1){
-          if(sql_args && sql_args[0]){
-            if(sql_args[0].toUpperCase().search('ERROR')>-1){
-              return callback("Database Error");
-            }
+    query: function(){
+      var sql_args = [];
+      var args = [];
+      for(var i=0; i<arguments.length; i++){
+        args.push(arguments[i]);
+      }
+      var callback = args[args.length-1]; //last arg is callback
+      if(args.length > 2){
+        var sql_args = args[1];
+      }
+      if(args[0].toUpperCase().search('SELECT ID') >-1){
+
+        if(sql_args && sql_args[0]){
+          if(sql_args[0].toUpperCase().search('ERROR')>-1){
+            return callback("Database Error");
           }
-          return callback(null, [{ID:1, Name: 'testing', Type:'mysql', Host: 'localhost', Port:8080, SAUser:'sauser'}]);
         }
-        if(args[0].toUpperCase().search('SELECT NAME') >-1){
-          if(sql_args && sql_args[0]){
-            if(sql_args[0].toUpperCase().search('ERROR')>-1){
-              return callback("Database Error");
-            }
+        return callback(null, [{ID:1, Name: 'testing', Type:'mysql', Host: 'localhost', Port:8080, SAUser:'sauser'}]);
+      }
+      else if(args[0].toUpperCase().search('SELECT NAME') >-1){
+
+        if(sql_args && sql_args[0]){
+          if(sql_args[0].toUpperCase().search('ERROR')>-1){
+            return callback("Database Error");
           }
-          return callback(null, [{ID:1, Name: 'testing', Type:'mysql', Host: 'localhost', Port:8080, SAUser:'sauser'}]);
         }
-        if(args[0].toUpperCase().search('SELECT *') >-1){
-          if(sql_args && sql_args[0]){
-            if(sql_args[0].toUpperCase().search('ERROR1')>-1){
-              return callback("Database Error");
-            }
-            if(sql_args[0].toUpperCase().search('ERROR4')>-1){
-              return callback(null, [{ID:1, Name: 'error', Type:'mysql', Host: 'localhost', Port:8080, SAUser:'sauser', SAPass: 'password'}]);
-            }
+        return callback(null, [{ID:1, Name: 'testing', Type:'mysql', Host: 'localhost', Port:8080, SAUser:'sauser'}]);
+      }
+      else if(args[0].toUpperCase().search('SELECT *') >-1){
+
+        if(sql_args && sql_args[0]){
+          if(sql_args[0].toUpperCase().search('ERROR1')>-1){
+            return callback("Database Error");
           }
-          return callback(null, [{ID:1, Name: 'testing', Type:'mysql', Host: 'localhost', Port:8080, SAUser:'sauser', SAPass: 'password'}]);
-        }
-        if(args[0].toUpperCase().search('DELETE FROM GROUPS_') >-1){
-          if(sql_args && sql_args[0]){
-            if(sql_args[0].toUpperCase().search('ERROR2')>-1){
-              return callback("Database Error");
-            }
+          if(sql_args[0].toUpperCase().search('ERROR4')>-1){
+            return callback(null, [{ID:1, Name: 'error', Type:'mysql', Host: 'localhost', Port:8080, SAUser:'sauser', SAPass: 'password'}]);
           }
-          return callback(null, [{ID:1, Name: 'testing', Type:'mysql', Host: 'localhost', Port:8080, SAUser:'sauser', SAPass: 'password'}]);
         }
-        if(args[0].toUpperCase().search('DELETE') >-1){
-          if(sql_args && sql_args[0]){
-            if(sql_args[0].toUpperCase().search('ERROR3')>-1){
-              return callback("Database Error");
-            }
+        return callback(null, [{ID:1, Name: 'testing', Type:'mysql', Host: 'localhost', Port:8080, SAUser:'sauser', SAPass: 'password'}]);
+      }
+      else if(args[0].toUpperCase().search('DELETE FROM GROUPS_') >-1){
+
+        if(sql_args && sql_args[0]){
+          if(sql_args[0].toUpperCase().search('ERROR2')>-1){
+            return callback("Database Error");
           }
-          return callback(null, [{ID:1, Name: 'testing', Type:'mysql', Host: 'localhost', Port:8080, SAUser:'sauser', SAPass: 'password'}]);
         }
-        if(args[0].toUpperCase().search('HISTORY')>-1){
-          console.log(sql_args);
-          if(sql_args && sql_args[0]){
-            if(sql_args[0].toUpperCase().search('ERROR')>-1){
-              return callback("Database Error");
-            }
+        return callback(null, [{ID:1, Name: 'testing', Type:'mysql', Host: 'localhost', Port:8080, SAUser:'sauser', SAPass: 'password'}]);
+      }
+      else if(args[0].toUpperCase().search('DELETE FROM `DATABASES`') >-1){
+
+        if(sql_args && sql_args[0]){
+          if(sql_args[0].toUpperCase().search('ERROR3')>-1){
+            return callback("Database Error");
           }
-          return callback(null, [{ID:1, Name: 'testing', Type:'mysql', Host: 'localhost', Port:8080, SAUser:'sauser', SAPass: 'password'}]);
         }
+        return callback(null, [{ID:1, Name: 'testing', Type:'mysql', Host: 'localhost', Port:8080, SAUser:'sauser', SAPass: 'password'}]);
+      }
+      else if(args[0].toUpperCase().search('INSERT INTO `DATABASES`') >-1){
+        if(sql_args && sql_args[0]){
+          if(sql_args[0].toUpperCase().search('ERROR1')>-1){
+            return callback("Database Error");
+          }
+          if(sql_args[0].toUpperCase().search('ERROR2')>-1){
+            return callback(null, {insertId:-1});
+          }
+        }
+        return callback(null, {insertId:1});
+      }
+      else if(args[0].toUpperCase().search('INSERT INTO GROUPS_DATABASES') >-1){
+        if(sql_args && sql_args[0]){
+          if(sql_args[0]===-1){
+            return callback("Database Error");
+          }
+        }
+        return callback(null, {insertId:1});
+      }
+      else if(args[0].toUpperCase().search('HISTORY') >-1){
+        if(sql_args && sql_args[0]){
+          if(sql_args[0].toUpperCase().search('ERROR')>-1){
+            return callback("Database Error");
+          }
+        }
+        return callback(null, {insertId:1});
+      }
+      else{
+        return callback("DB Error");
       }
     }
+  }
 
   var mock_encrypt = {
     encrypt: function(plaintext, callback){
@@ -106,10 +132,12 @@ describe.only('databases', function(){
   var db_revert;
   var enc_revert;
   var tools_revert;
+  var quiet_revert;
   before(function(){
     db_revert = databases.__set__('connection', mock_db);
     enc_revert = databases.__set__('encryption', mock_encrypt);
     tools_revert = databases.__set__('db_tools', mock_db_tools);
+    quiet_revert = databases.__set__('console', {log:function(){}});
   });
   describe('GET /', function(){
     it('should return an error on db error', function(done){
@@ -201,102 +229,182 @@ describe.only('databases', function(){
     });
   });
   describe('DELETE /:id', function(){
-    describe('Database Error chain', function(){
-      it('should error on database error - select', function(done){
-        request(app)
-        .delete('/api/databases/error1')
-        .expect(200)
-        .end(function(err, res){
-          if(err){
-            console.log(err);
-            return done(err);
-          }
-          assert.equal(res.body.Success, false, 'Successful request despite error');
-          assert(res.body.Error, 'No Error on DB Error');
-          done();
-        });
-      });
-      it('should error on database error - delete g_d', function(done){
-        request(app)
-        .delete('/api/databases/error2')
-        .expect(200)
-        .end(function(err, res){
-          if(err){
-            console.log(err);
-            return done(err);
-          }
-          assert.equal(res.body.Success, false, 'Successful request despite error');
-          assert(res.body.Error, 'No Error on DB Error');
-          done();
-        });
-      });
-      it('should error on database error - delete databases', function(done){
-        request(app)
-        .delete('/api/databases/error3')
-        .expect(200)
-        .end(function(err, res){
-          if(err){
-            console.log(err);
-            return done(err);
-          }
-          assert.equal(res.body.Success, false, 'Successful request despite error');
-          assert(res.body.Error, 'No Error on DB Error');
-          done();
-        });
-      });
-      it('should error on database error - insert', function(done){
-        request(app)
-        .delete('/api/databases/error4')
-        .expect(200)
-        .end(function(err, res){
-          if(err){
-            console.log(err);
-            return done(err);
-          }
-          assert(res.body.Error, 'No Error on DB Error');
-          done();
-        });
-      });
-    });
-  });
-  /*
-  describe('#add_database()', function(){
-    var add_database = databases.__get__('add_database');
-    it('should fail if called without all data', function(done){
-      add_database({}, function(err, result){
-        assert(err, 'No error despite no info passed');
+    it('should error on database error - select', function(done){
+      request(app)
+      .delete('/api/databases/error1')
+      .expect(200)
+      .end(function(err, res){
+        if(err){
+          console.log(err);
+          return done(err);
+        }
+        assert.equal(res.body.Success, false, 'Successful request despite error');
+        assert(res.body.Error, 'No Error on DB Error');
         done();
       });
     });
-    it('should fail on duplicate Hosts');
-    it('should return an ID of the inserted DB');
-  });
-  describe('#update_database()', function(){
-    var update_database = databases.__get__('update_database');
-    it('should fail if called without all data', function(done){
-      update_database({}, function(err, result){
-        assert(err, 'No error despite no info passed');
+    it('should error on database error - delete g_d', function(done){
+      request(app)
+      .delete('/api/databases/error2')
+      .expect(200)
+      .end(function(err, res){
+        if(err){
+          console.log(err);
+          return done(err);
+        }
+        assert.equal(res.body.Success, false, 'Successful request despite error');
+        assert(res.body.Error, 'No Error on DB Error');
         done();
       });
     });
-    it('should fail if no ID is sent');
-    it('should return an ID on success');
-  });
-  describe('#remove_database()', function(){
-    var remove_database = databases.__get__('remove_database');
-    it('should fail if called without all data', function(done){
-      remove_database({}, function(err, result){
-        assert(err, 'No error despite no info passed');
+    it('should error on database error - delete databases', function(done){
+      request(app)
+      .delete('/api/databases/error3')
+      .expect(200)
+      .end(function(err, res){
+        if(err){
+          console.log(err);
+          return done(err);
+        }
+        assert.equal(res.body.Success, false, 'Successful request despite error');
+        assert(res.body.Error, 'No Error on DB Error');
         done();
       });
     });
-    it('should fail if no database has that ID');
-    it('should return success if it works');
+    it('should error on database error - insert', function(done){
+      request(app)
+      .delete('/api/databases/error4')
+      .expect(200)
+      .end(function(err, res){
+        if(err){
+          console.log(err);
+          return done(err);
+        }
+        assert(res.body.Error, 'No Error on DB Error');
+        done();
+      });
+    });
+    it('should return successfully on valid ID', function(done){
+      request(app)
+      .delete('/api/databases/7')
+      .expect(200, {Success: true}, done);
+    });
   });
-  */
+  describe('POST /', function(){
+    it('should fail if called without all data', function(done){
+      request(app)
+      .post('/api/databases/')
+      .set('Content-Type', 'application/json')
+      .send({})
+      .expect(200)
+      .end(function(err, res){
+        if(err){
+          return done(err);
+        }
+        assert.equal(res.body.Success, false, 'Successful post with no data');
+        assert(res.body.Error, 'No Error on null body');
+        done();
+      });
+    });
+    describe('add database', function(){
+      it('should fail on db error - insert database', function(done){
+        request(app)
+        .post('/api/databases/')
+        .set('Content-Type', 'application/json')
+        .send({Name: 'error1', Type: 'mysql', Host:'localhost', Port: 8080, SAUser:'sauser', SAPass: 'somepass'})
+        .expect(200)
+        .end(function(err, res){
+          if(err){
+            return done(err);
+          }
+          assert.equal(res.body.Success, false, 'Successful post despite db error');
+          assert(res.body.Error, 'No Error on DB Error');
+          done();
+        });
+      });
+      it('should fail on db error - insert groups_databases', function(done){
+        request(app)
+        .post('/api/databases/')
+        .set('Content-Type', 'application/json')
+        .send({Name: 'error2', Type: 'mysql', Host:'localhost', Port: 8080, SAUser:'sauser', SAPass: 'somepass'})
+        .expect(200)
+        .end(function(err, res){
+          if(err){
+            return done(err);
+          }
+          assert.equal(res.body.Success, false, 'Successful post despite db error');
+          assert(res.body.Error, 'No Error on DB Error');
+          done();
+        });
+      });
+      it('should still work on history error', function(done){
+        request(app)
+        .post('/api/databases/')
+        .set('Content-Type', 'application/json')
+        .send({Name: 'error', Type: 'mysql', Host:'localhost', Port: 8080, SAUser:'sauser', SAPass: 'somepass'})
+        .expect(200)
+        .end(function(err, res){
+          if(err){
+            return done(err);
+          }
+          assert(res.body.Success, 'Unsuccessful post');
+          assert(res.body.ID, 'No ID Returned');
+          done();
+        });
+      });
+    });
+    describe('update database', function(){
+      it('should fail on db error - insert database', function(done){
+        request(app)
+        .post('/api/databases/')
+        .set('Content-Type', 'application/json')
+        .send({ID:-1, Name: 'error1', Type: 'mysql', Host:'localhost', Port: 8080, SAUser:'sauser', SAPass: 'somepass'})
+        .expect(200)
+        .end(function(err, res){
+          if(err){
+            return done(err);
+          }
+          assert.equal(res.body.Success, false, 'Successful post despite db error');
+          assert(res.body.Error, 'No Error on DB Error');
+          done();
+        });
+      });
+      it('should still work on history error', function(done){
+        request(app)
+        .post('/api/databases/')
+        .set('Content-Type', 'application/json')
+        .send({ID:1, Name: 'error', Type: 'mysql', Host:'localhost', Port: 8080, SAUser:'sauser', SAPass: 'somepass'})
+        .expect(200)
+        .end(function(err, res){
+          if(err){
+            return done(err);
+          }
+          assert(res.body.Success, 'Unsuccessful post');
+          assert(res.body.ID, 'No ID Returned');
+          done();
+        });
+      });
+      it('return the ID of the affected database',function(done){
+        request(app)
+        .post('/api/databases/')
+        .set('Content-Type', 'application/json')
+        .send({ID:1, Name: 'test', Type: 'mysql', Host:'localhost', Port: 8080, SAUser:'sauser', SAPass: 'somepass'})
+        .expect(200)
+        .end(function(err, res){
+          if(err){
+            return done(err);
+          }
+          assert(res.body.Success, 'Unsuccessful post');
+          assert(res.body.ID, 'No ID Returned');
+          done();
+        });
+      });
+    });
+  });
   after(function(){
     db_revert();
     enc_revert();
     tools_revert();
+    quiet_revert
   });
 });
