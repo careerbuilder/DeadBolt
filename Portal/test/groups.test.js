4ab43720a85793dec83d3294cec9c4d768a28d03
Adam Yost <Adam.Yost@Careerbuilder.com>
2015-07-29 07:36:56 -0400

    increase code coverage for groups

--- Portal/test/groups.test.js
+++ Portal/test/groups.test.js
@@ -72,6 +72,12 @@ describe('groups', function(){
       }
       if(args[0].toUpperCase().search('INSERT INTO HISTORY')>-1){
         if(sql_args && sql_args[0]){
+          if(sql_args[0] == 1){
+            return callback(null, {insertId: 1});
+          }
+          if(sql_args[0] == -8){
+            return callback("Database Error");
+          }
           if(sql_args[0].toUpperCase().search('HISTERROR')>-1){
             return callback("Database Error");
           }
@@ -93,6 +99,30 @@ describe('groups', function(){
           }
         }
         return callback(null, [{ID:1, Name: 'testuser'}, {ID:2, Name:'testuser2'}]);
+      }
+      if(args[0].toUpperCase().search('FROM `DATABASES` WHERE ID')>-1){
+        if(sql_args && sql_args[0]){
+          if(sql_args[0]==-1){
+            return callback("Database Error");
+          }
+        }
+        return callback(null, [{ID:1, Name: 'testuser'}, {ID:2, Name:'testuser2'}]);
+      }
+      if(args[0].toUpperCase().search('DELETE FROM USERS_GROUPS')>-1){
+        if(sql_args && sql_args[0]){
+          if(sql_args[0]==-6){
+            return callback("Database Error");
+          }
+        }
+        return callback();
+      }
+      if(args[0].toUpperCase().search('DELETE FROM GROUPS')>-1){
+        if(sql_args && sql_args[0]){
+          if(sql_args[0]==-7){
+            return callback("Database Error");
+          }
+        }
+        return callback();
       }
     }
   }
@@ -373,7 +403,102 @@ describe('groups', function(){
     });
   });
   describe('DELETE /', function(){
-
+    it('should error on DB Error - get db1', function(done){
+      request(app)
+      .delete('/api/groups/-1')
+      .set('Content-Type', 'application/json')
+      .expect(200)
+      .end(function(err, res){
+        if(err){
+          console.log(err);
+          return done(err);
+        }
+        assert.equal(res.body.Success, false, 'Successful despite db error');
+        assert(res.body.Error, 'No Error on db error');
+        done();
+      });
+    });
+    it('should error on DB Error - get users', function(done){
+      request(app)
+      .delete('/api/groups/-5')
+      .set('Content-Type', 'application/json')
+      .expect(200)
+      .end(function(err, res){
+        if(err){
+          console.log(err);
+          return done(err);
+        }
+        assert.equal(res.body.Success, false, 'Successful despite db error');
+        assert(res.body.Error, 'No Error on db error');
+        done();
+      });
+    });
+    it('should error on DB Error - delete g_d', function(done){
+      request(app)
+      .delete('/api/groups/-3')
+      .set('Content-Type', 'application/json')
+      .expect(200)
+      .end(function(err, res){
+        if(err){
+          console.log(err);
+          return done(err);
+        }
+        assert.equal(res.body.Success, false, 'Successful despite db error');
+        assert(res.body.Error, 'No Error on db error');
+        done();
+      });
+    });
+    it('should error on DB Error - delete u_g', function(done){
+      request(app)
+      .delete('/api/groups/-6')
+      .set('Content-Type', 'application/json')
+      .expect(200)
+      .end(function(err, res){
+        if(err){
+          console.log(err);
+          return done(err);
+        }
+        assert.equal(res.body.Success, false, 'Successful despite db error');
+        assert(res.body.Error, 'No Error on db error');
+        done();
+      });
+    });
+    it('should error on DB Error - delete groups', function(done){
+      request(app)
+      .delete('/api/groups/-7')
+      .set('Content-Type', 'application/json')
+      .expect(200)
+      .end(function(err, res){
+        if(err){
+          console.log(err);
+          return done(err);
+        }
+        assert.equal(res.body.Success, false, 'Successful despite db error');
+        assert(res.body.Error, 'No Error on db error');
+        done();
+      });
+    });
+    it('should succeed on history error', function(done){
+      request(app)
+      .delete('/api/groups/-8')
+      .set('Content-Type', 'application/json')
+      .expect(200)
+      .end(function(err, res){
+        if(err){
+          console.log(err);
+          return done(err);
+        }
+        assert(res.body.Success, 'Unsuccessful despite history error');
+        assert(res.body.Error, 'No Error on history error');
+        done();
+      });
+    });
+    it('should succeed on valid input', function(done){
+      request(app)
+      .delete('/api/groups/1')
+      .set('Content-Type', 'application/json')
+      .expect(200, {Success:true}, done);
+    });
   });
   after(function(){
     db_revert();
