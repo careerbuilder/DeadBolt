518035bd7c7e4391824d99825085ff993bd4ed58
Adam Yost <Adam.Yost@Careerbuilder.com>
2015-08-12 11:19:10 -0400

    actually learned how limits work

--- Portal/routes/users.js
+++ Portal/routes/users.js
@@ -136,20 +136,19 @@ router.post('/search/:page', function(req, res){
   var body = req.body;
   var page = req.params.page;
   var start=  page * 50;
-  var end  = (start + 50)-1;
   var query = "";
   var count_query = "";
   var args = [];
   if(body.Info && body.Info.trim().length > 0){
     var info = "%"+body.Info+"%";
     count_query = 'Select Count(*) as Total from possible_users where (Username like ? OR Email like ? OR FirstName like ? OR LastName like ?);';
-    query = 'Select ID, Username, Email, User_ID, FirstName, LastName from possible_users where (Username like ? OR Email like ? OR FirstName like ? OR LastName like ?) ORDER BY if(User_ID = "" or User_ID is null,1,0),User_ID, Username ASC LIMIT ?,?;';
-    args = [info, info, info, info, start, end];
+    query = 'Select ID, Username, Email, User_ID, FirstName, LastName from possible_users where (Username like ? OR Email like ? OR FirstName like ? OR LastName like ?) ORDER BY if(User_ID = "" or User_ID is null,1,0),User_ID, Username ASC LIMIT ?,50;';
+    args = [info, info, info, info, start];
   }
   else{
     count_query = 'Select Count(*) as Total from possible_users;';
-    query = 'Select ID, Username, Email, User_ID, FirstName, LastName from possible_users ORDER BY if(User_ID = "" or User_ID is null,1,0),User_ID, Username ASC LIMIT ?, ?;';
-    args = [start, end];
+    query = 'Select ID, Username, Email, User_ID, FirstName, LastName from possible_users ORDER BY if(User_ID = "" or User_ID is null,1,0),User_ID, Username ASC LIMIT ?, 50;';
+    args = [start];
   }
   connection.query(count_query, args, function(err, results){
     if(err){
