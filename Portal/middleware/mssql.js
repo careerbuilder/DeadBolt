0fe7118d4e391cdaeb25bcc169265380e3e8785b
Adam Yost <Adam.Yost@Careerbuilder.com>
2015-09-03 13:29:25 -0400

    Bundle mssql login into middleware to avoid transaction errors

--- Portal/middleware/mssql.js
+++ Portal/middleware/mssql.js
@@ -1,34 +1,40 @@
 var mssql = require('mssql');
 
 module.exports = {
-  query: function(conn, sql, callback){
-    var trans = new mssql.Transaction(conn);
-    trans.begin(function(err){
+  connect: function(config, callback){
+    var connection = new mssql.Connection(config, function(err){
       if(err){
         return callback(err);
       }
-      var rolledBack = false;
-      trans.on('rollback', function(aborted) {
-        rolledBack = true;
-      });
-      var request = new mssql.Request(trans);
+      return callback(null, connection);
+    });
+  },
+  query: function(conn, sql, callback){
+    var trans = conn.transaction();
+    trans.begin(function(err){
+      if(err){
+        console.log('Error beginning transaction');
+        return callback(err);
+      }
+      var request = trans.request();
       request.query(sql, function(err, records){
         if(err){
           var sql_error = err;
-          if(!rolledBack){
-            trans.rollback(function(error){
-              if(error){
-                console.log('Error rolling back transaction: ',error);
-              }
-              return callback(sql_error);
-            });
-          }
+          trans.rollback(function(error){
+            if(error){
+              console.log('Error rolling back transaction: ',error);
+            }
+            console.log('Transaction rolled back');
+            return callback(sql_error);
+          });
         }
         else{
           trans.commit(function(err) {
               if(err){
+                console.log('Error committing transaction');
                 return callback(err);
               }
+              console.log('Transaction committed');
               return callback(null, records);
           });
         }
