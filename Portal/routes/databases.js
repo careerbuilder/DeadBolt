f01f576c81554d81db08845aad16dea595e874e3
Adam Yost <Adam.Yost@Careerbuilder.com>
2015-12-16 14:35:57 -0500

    properly store and retrieve SSL settings (requires DB change)

--- Portal/routes/databases.js
+++ Portal/routes/databases.js
@@ -5,41 +5,56 @@ var connection = require('../middleware/mysql');
 var encryption = require('../middleware/encryption');
 var db_tools = require('../tools/db_tools');
 
+function tinyIntToBoolean(field, next){
+  if (field.type == 'TINY' && field.length == 1) {
+    return (field.string() == '1'); // 1 = true, 0 = false
+  }
+  return next();
+};
+
 function add_database(body, callback){
   encryption.encrypt(body.SAPass, function(err, data){
     if(err){
       return callback(err);
     }
     var sacreds = data;
-    var args = [body.Name, body.Type, body.Host, body.Port, body.SAUser, sacreds];
-    var query = 'Insert into `databases` (Name, Type, Host, Port, SAUser, SAPass) values (?, ?, ?, ?, ?, ?) On Duplicate key Update Name=Values(name), Host=Values(Host), Port=Values(Port), Type=Values(Type), SAUser=Values(SAUser), SAPass=Values(SAPass);';
+    var args = [body.Name, body.Type, body.Host, body.Port, body.SAUser, sacreds, body.ForceSSL];
+    var query = 'Insert into `databases` (Name, Type, Host, Port, SAUser, SAPass, ForceSSL) values (?, ?, ?, ?, ?, ?, ?) On Duplicate key Update Name=Values(name), Host=Values(Host), Port=Values(Port), Type=Values(Type), SAUser=Values(SAUser), SAPass=Values(SAPass), ForceSSL=Values(ForceSSL);';
     connection.query(query, args, function(err, result){
       if(err){
         return callback(err);
       }
-      var DB_ID = result.insertId;
-      body.ID = DB_ID;
-      connection.query("Insert into groups_databases (Group_ID, Database_ID) Values(-1, ?) ON DUPLICATE KEY UPDATE Database_ID=Database_ID;", [DB_ID], function(err, result){
+      connection.query('Select ID from `databases` where Host=? and Port=? and Name=?', [body.Host, body.Port, body.Name], function(err, results){
         if(err){
           return callback(err);
         }
-        var dbinfo = {Name: body.Name, Type: body.Type, Host: body.Host, Port: body.Port, SAUser: body.SAUser, SAPass: sacreds, ID:DB_ID};
-        db_tools.update_all_users(dbinfo, function(err, results){});
-        connection.query('Insert into History (Activity) Value("Added Database: ?")', [body.Name], function(err, result){
+        if(results.length<1){
+          return callback('No such server!');
+        }
+        var DB_ID = results[0].ID;
+        body.ID = DB_ID;
+        connection.query("Insert into groups_databases (Group_ID, Database_ID) Values(-1, ?) ON DUPLICATE KEY UPDATE Database_ID=Database_ID;", [DB_ID], function(err, result){
           if(err){
-            console.log(err);
+            return callback(err);
           }
-          return callback(null, DB_ID);
+          var dbinfo = {Name: body.Name, Type: body.Type, Host: body.Host, Port: body.Port, SAUser: body.SAUser, SAPass: sacreds, ForceSSL:body.ForceSSL, ID:DB_ID};
+          db_tools.update_all_users(dbinfo, function(err, results){});
+          connection.query('Insert into History (Activity) Value("Added Database: ?")', [body.Name], function(err, result){
+            if(err){
+              console.log(err);
+            }
+            return callback(null, DB_ID);
+          });
         });
       });
     });
   });
-}
+};
 
 function update_database(body, callback){
   var DB_ID = body.ID;
-  var args = [body.Name, body.Type, body.Host, body.Port, body.SAUser];
-  var query = 'Update `databases` SET Name = ?, Type = ?, Host = ?, Port = ?, SAUser = ?';
+  var args = [body.Name, body.Type, body.Host, body.Port, body.SAUser, body.ForceSSL];
+  var query = 'Update `databases` SET Name = ?, Type = ?, Host = ?, Port = ?, SAUser = ?, ForceSSL=?';
   var sacreds = undefined;
   async.series([
     function(cb){
@@ -74,7 +89,7 @@ function update_database(body, callback){
           console.log(err);
           return cb(err);
         }
-        var dbinfo = {Name: body.Name, Type: body.Type, Host: body.Host, Port: body.Port, SAUser: body.SAUser, SAPass: sacreds, ID:DB_ID};
+        var dbinfo = {Name: body.Name, Type: body.Type, Host: body.Host, Port: body.Port, SAUser: body.SAUser, SAPass: sacreds, ForceSSL:body.ForceSSL, ID:DB_ID};
         db_tools.update_all_users(dbinfo, function(err, results){});
         connection.query('Insert into History (Activity) Value("Edited Database: ?")', [body.Name], function(err, result){
           if(err){
@@ -91,7 +106,7 @@ function update_database(body, callback){
     }
     return callback(null, DB_ID);
   });
-}
+};
 
 router.post('/', function(req, res){
   var body = req.body;
@@ -116,7 +131,11 @@ router.post('/', function(req, res){
 });
 
 router.get('/', function(req, res){
-  connection.query('Select ID, Name, Type, Host, Port from `databases`;', function(err, results){
+  var opts = {
+    sql:'Select ID, Name, Type, Host, Port, ForceSSL from `databases`;',
+    typeCast: tinyIntToBoolean
+  };
+  connection.query(opts, function(err, results){
     if(err){
       console.log(err);
       return res.send({Success:false, Error:err});
@@ -131,13 +150,18 @@ router.post('/search', function(req, res){
   var args = [];
   if(body && body.Info && body.Info.trim().length > 0){
     var info = "%"+body.Info+"%";
-    query = 'Select ID, Name, Type, Host, Port, SAUser from `databases` where (Name like ? OR Type like ? OR Host like ? OR Port like ? OR SAUser like ?);';
+    query = 'Select ID, Name, Type, Host, Port, SAUser, ForceSSL from `databases` where (Name like ? OR Type like ? OR Host like ? OR Port like ? OR SAUser like ?);';
     args = [info, info, info, info, info];
   }
   else{
-    query = 'Select ID, Name, Type, Host, Port, SAUser from `databases`;';
+    query = 'Select ID, Name, Type, Host, Port, SAUser, ForceSSL from `databases`;';
   }
-  connection.query(query, args, function(err, results){
+  var opts = {
+    sql:query,
+    typeCast: tinyIntToBoolean,
+    values:args
+  };
+  connection.query(opts, function(err, results){
     if(err){
       console.log(err);
       return res.send({Success: false, Error: err});
@@ -164,7 +188,12 @@ router.get('/:groupname', function(req, res){
 
 router.delete('/:id', function(req,res){
   var db_id = req.params.id;
-  connection.query("Select * from `databases` where ID = ? LIMIT 1;", [db_id], function(err, data){
+  var opts = {
+    sql:"Select * from `databases` where ID = ? LIMIT 1;",
+    values: [db_id],
+    typeCast: tinyIntToBoolean
+  };
+  connection.query(opts, function(err, data){
     if(err){
       console.log(err);
       return res.send({Success: false, Error: err});
