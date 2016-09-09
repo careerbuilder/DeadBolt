fc3dbd8dd3e470a0cd9649833d97637b487b9d47
Garrett Delfosse <f0ssel@users.noreply.github.com>
2016-09-09 16:39:20 -0400

    Update example.config.js

--- client/example.config.js
+++ client/example.config.js
@@ -11,6 +11,6 @@
 * See the License for the specific language governing permissions and limitations under the License.
 */
 app.constant('globalConfig', {
-  contactEmail: ''
+  contactEmail: 'example@example.com'
 });
 
